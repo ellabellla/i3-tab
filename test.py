41da03db35ec5b5414689865008d68e42c50ce28
ellabellla <86757313+ellabellla@users.noreply.github.com>
2022-03-15 17:03:30 +1100

    + basic window manager funcs & tests

---
+++ test.py
@@ -0,0 +1,52 @@
+import unittest
+import wm
+
+class WMTest(unittest.TestCase):
+    def test_add_window(self):
+        window_manager = wm.WindowManager(testing=True)
+        
+        window_manager.add_window({'id':'1', 'name':'first'})
+        window_manager.add_window({'id':'2', 'name':'second'})
+        window_manager.add_window({'id':'3', 'name':'third'})
+        
+        self.assertEqual([{'id':'3', 'name':'third'}, {'id':'2', 'name':'second'},{'id':'1', 'name':'first'}], list(window_manager._windows))
+    
+    
+    def test_remove_window(self):
+        window_manager = wm.WindowManager(testing=True)
+        
+        window_manager._windows = [{'id':'3', 'name':'third'}, {'id':'2', 'name':'second'},{'id':'1', 'name':'first'}]
+        window_manager.remove_window('2')
+        
+        self.assertEqual([{'id':'3', 'name':'third'}, {'id':'1', 'name':'first'}], list(window_manager._windows))
+    
+    def test_move_to_front(self):
+        window_manager = wm.WindowManager(testing=True)
+        
+        window_manager._windows = [{'id':'3', 'name':'third'}, {'id':'2', 'name':'second'},{'id':'1', 'name':'first'}]
+        window_manager.move_to_front('1')
+        
+        self.assertEqual([{'id':'1', 'name':'first'}, {'id':'3', 'name':'third'}, {'id':'2', 'name':'second'}], list(window_manager._windows))
+    
+    def test_flip(self):
+        window_manager = wm.WindowManager(testing=True)
+        
+        window_manager._windows = [{'id':'3', 'name':'third'}, {'id':'2', 'name':'second'},{'id':'1', 'name':'first'}]
+        window_manager.flip()
+        
+        self.assertEqual([{'id':'2', 'name':'second'}, {'id':'3', 'name':'third'}, {'id':'1', 'name':'first'}], list(window_manager._windows))
+    
+    def test_front(self):
+        window_manager = wm.WindowManager(testing=True)
+        
+        window_manager.add_window({'id':'1', 'name':'first'})
+        self.assertEqual({'id':'1', 'name':'first'}, window_manager.front())
+        
+        window_manager.add_window({'id':'2', 'name':'second'})
+        self.assertEqual({'id':'2', 'name':'second'}, window_manager.front())
+        
+        window_manager.add_window({'id':'3', 'name':'third'})
+        self.assertEqual({'id':'3', 'name':'third'}, window_manager.front())
+        
+if __name__ == '__main__':
+    unittest.main()
--- wm.py
+++ wm.py
@@ -1,4 +1,79 @@
+from i3ipc import Connection, Event
+from threading import Lock, Thread
+from itertools import chain
 
+class WindowManager():
+    def __init__(self, testing=False):
+        self._windows = []
+        self._lock = Lock()
+        
+        if testing:
+            self._testing = True
+            return
+        else:
+            self._testing = False
+            
+        self._i3 = Connection(auto_reconnect=True)
+        
+        self._i3.on(Event.WINDOW_NEW, self._create_on_window_new)
+        self._i3.on(Event.WINDOW_CLOSE, self._create_on_window_close)
+        
+        self._thread = Thread(target=self._i3.main)
+        self._thread.start()
 
-def run():
-    pass
+    def front(self):            
+        self._lock.acquire()
+        front = None
+        try:
+            front = None if len(self._windows) == 0 else self._windows[0]
+        finally:
+            self._lock.release()
+            return front
+
+    def remove_window(self, id):
+        self._lock.acquire()
+        try:
+            self._windows = filter(lambda window: window['id'] != id, self._windows)
+        finally:
+            self._lock.release()
+
+    def add_window(self, con):
+        self._lock.acquire()
+        try:
+            self._windows = [{'id': con['id'], 'name': con['name']}] + self._windows
+        finally:
+            self._lock.release()
+
+    def move_to_front(self, id):
+        self._lock.acquire()
+        try:
+            window  = list(filter(lambda window: window['id'] == id, self._windows))
+            
+            if len(window) == 0:
+                return
+            
+            self._windows = chain(window, filter(lambda window: window['id'] != id, self._windows))
+        finally:
+            self._lock.release()
+            
+    def flip(self):
+        if len(self._windows) < 2:
+            return    
+        
+        self._lock.acquire()
+        try:
+            self._windows = [self._windows[1]] + [self._windows[0]] + self._windows[2:]
+        finally:
+            self._lock.release()
+
+    def _create_on_window_new(self, e):
+        def closure(connection, e):
+            self.add_window(e.container)
+        
+        return closure
+
+    def _create_on_window_close(self, e):
+        def closure(connection, e):
+            self.remove_window(e.container)
+        
+        return closure
